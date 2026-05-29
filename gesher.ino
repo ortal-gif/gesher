0b5cb7dee203e80777504cb7692b3bc978ceca61
ortal-gif <ortalk2019@gmail.com>
2026-05-29 12:08:08 +0300

    Update gesher.ino

--- gesher.ino
+++ gesher.ino
@@ -9,11 +9,10 @@ const int motorEN1 = 3;
 const int motorEN2 = 9;
 
 // Sensor pins
-//const int trigPin = 12;
-//const int echoPin = 11;
-//const int ldrPin = A0;
+const int trigPin = 12;
+const int echoPin = 11;
 
-
+const long STOP_DIST_CM = 10;
 
 
 void setup() {
@@ -27,24 +26,42 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(motorIN1, LOW);
-  digitalWrite(motorIN2, HIGH);
-  digitalWrite(motorIN3, LOW);
-  digitalWrite(motorIN4, HIGH);
-  analogWrite(motorEN1, 80);
-  analogWrite(motorEN2, 80);
-  delay(1200);
-  analogWrite(motorEN1, 0);
-  analogWrite(motorEN2, 0);
-  delay(2000);
-  digitalWrite(motorIN1, HIGH);
-  digitalWrite(motorIN2, LOW);
-  digitalWrite(motorIN3, HIGH);
-  digitalWrite(motorIN4, LOW);
-  analogWrite(motorEN1, 80);
-  analogWrite(motorEN2, 80);
-  delay(1200);
-  analogWrite(motorEN1, 0);
-  analogWrite(motorEN2, 0);
-  delay(2000);
+  long dist = readUltrasonicCM();
+
+  Serial.print("  Dist: "); Serial.println(dist);
+  
+  if (dist > 0 && dist <= STOP_DIST_CM) {
+    digitalWrite(motorIN1, LOW);
+    digitalWrite(motorIN2, HIGH);
+    digitalWrite(motorIN3, LOW);
+    digitalWrite(motorIN4, HIGH);
+    analogWrite(motorEN1, 80);
+    analogWrite(motorEN2, 80);
+    delay(1200);
+    analogWrite(motorEN1, 0);
+    analogWrite(motorEN2, 0);
+    delay(2000);
+    digitalWrite(motorIN1, HIGH);
+    digitalWrite(motorIN2, LOW);
+    digitalWrite(motorIN3, HIGH);
+    digitalWrite(motorIN4, LOW);
+    analogWrite(motorEN1, 80);
+    analogWrite(motorEN2, 80);
+    delay(1200);
+    analogWrite(motorEN1, 0);
+    analogWrite(motorEN2, 0);
+    delay(2000);}
+}
+
+//---ULTRASONIC HELPER---
+long readUltrasonicCM(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH, 30000);
+  if (duration == 0) return -1;
+  return duration / 29 / 2;
 }
